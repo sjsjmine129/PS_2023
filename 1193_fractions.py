6377c933943830338b05e4513a017bc9af1cd9e2
sjsjmine129 <sjsjmine129@gmail.com>
2023-09-20 22:05:17 +0900

    init

---
+++ 1193_fractions.py
@@ -0,0 +1,18 @@
+x = int(input())
+j = 0
+i = 1
+
+for i in range(1, x+1):
+    j += i
+    if j >= x:
+        break
+
+row = i+1
+
+gap = j-x+1
+other = row-gap
+
+if (i % 2 == 0):
+    print(str(other)+'/'+str(gap))
+else:
+    print(str(gap)+'/'+str(other))
