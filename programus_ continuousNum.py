fffae3c1772107a8a06d65ec7f9b82a63e860174
sjsjmine129 <sjsjmine129@gmail.com>
2024-04-01 17:56:08 +0900

    4/1

---
+++ programus_ continuousNum.py
@@ -0,0 +1,27 @@
+from collections import deque
+
+
+def solution(sequence, k):
+    answer = []
+    minlen = len(sequence)
+    start = 0
+    end = 0
+    temp = deque()
+    sum = 0
+
+    for i in range(len(sequence)):
+        now = sequence[i]
+        sum += now
+        temp.append(now)
+        end = i
+
+        while sum > k:
+            sum -= temp.popleft()
+            start += 1
+
+        if sum == k and end - start < minlen:
+            print(minlen)
+            minlen = end - start
+            answer = [start, end]
+
+    return answer
