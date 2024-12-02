d992c02bb335989f744ff4016d675c9401143f71
sjsjmine129 <sjsjmine129@gmail.com>
2024-12-02 15:41:49 +0900

    Fix

--- samsung_tests/binary/binary.c
+++ samsung_tests/binary/binary.c
@@ -1,17 +1,14 @@
 #include <stdio.h>
-#define MAXROW 50
-#define MAXCOL 100
-#define CODELEN 7 * 8
- 
-char input[MAXROW][MAXCOL];
-char code[CODELEN];
-char decrypt[127] = { 0, };
-int isFirst = 1;
- 
+
+
+char data[50][100];
+char code[56];
+char decrypt[127] = {0};
+
 void makeDecrypt() {
-    isFirst = 0;
     for (int i = 0; i < 127; i++)
         decrypt[i] = 0;
+
     decrypt[13] = 0;
     decrypt[25] = 1;
     decrypt[19] = 2;
@@ -23,13 +20,13 @@ void makeDecrypt() {
     decrypt[55] = 8;
     decrypt[11] = 9;
 }
-int verification(char arr[CODELEN]) {
- 
-    int decimal[8] = { 0, };
+
+int verification(char arr[56]) {
+    int decimal[8] = {0};
     int dIdx = 0;
- 
-    for (int i = 0; i < CODELEN; i++) {
-        if (i != 0 && i % 7 == 0) {
+
+    for (int i = 0; i < 56; i++) {
+        if (i % 7 == 0 && i != 0) {
             decimal[dIdx] = decrypt[decimal[dIdx]];
             dIdx++;
         }
@@ -37,62 +34,64 @@ int verification(char arr[CODELEN]) {
         decimal[dIdx] += (arr[i] - '0');
     }
     decimal[dIdx] = decrypt[decimal[dIdx]];
-    int odd = 0;    //홀수
-    int even = 0;   //짝수
-    int sum = 0;
+
+    int odd_sum = 0;
+    int even_sum = 0;
+    int total_sum = 0;
+
     for (int i = 0; i < 8; i++) {
-        if (i % 2 == 1)
-            odd += decimal[i];
+        if (i % 2 == 0)
+            even_sum += decimal[i];
         else
-            even += decimal[i];
-        sum += decimal[i];
+            odd_sum += decimal[i];
+        total_sum += decimal[i];
     }
- 
-    if (((even * 3) + odd) % 10 == 0)
-        return sum;
+
+    return (((even_sum * 3) + odd_sum) % 10 == 0) ? total_sum : 0;
+}
+
+int findCodeStart(int n, int m, int *row, int *col) {
+    for (int i = n - 1; i >= 0; i--) {
+        for (int j = m - 1; j >= 0; j--) {
+            if (data[i][j] == '1') {
+                *row = i;
+                *col = j - 56 + 1;
+                return 1;
+            }
+        }
+    }
     return 0;
 }
- 
-int main(void)
-{
-    int test_case;
+
+void extractCode(int row, int col) {
+    for (int i = 0; i < 56; i++) {
+        code[i] = data[row][col + i];
+    }
+}
+
+int main(void) {
     int T;
-    int n, m;
-    int result;
-    int row, col;
-    int flag;
- 
-    setbuf(stdout, NULL);
     scanf("%d", &T);
-    isFirst = 1;
-    for (test_case = 1; test_case <= T; ++test_case)
-    {
-        result = 0;
-        if (isFirst)
-            makeDecrypt();
-        flag = 0;
+    makeDecrypt(); 
+
+    for (int test_case = 1; test_case <= T; ++test_case) {
+        int n, m;
+        int result = 0;
         scanf("%d %d", &n, &m);
+
+        // Read the data grid
         for (int i = 0; i < n; i++) {
-            scanf("%s", input[i]);
+            scanf("%s", data[i]);
         }
-        for (int i = n - 1; i >= 0; i--) {
-            for (int j = m - 1; j >= 0; j--) {
-                if (input[i][j] == '1') {
-                    row = i;
-                    col = j - CODELEN + 1;
-                    flag = 1;
-                    break;
-                }
-                if (flag) break;
-            }
-            if (flag) break;
+
+        int row, col;
+        if (findCodeStart(n, m, &row, &col)) {
+            extractCode(row, col);      
+            result = verification(code);  
         }
-        for (int i = 0; i < CODELEN; i++) {
-            code[i] = input[row][col + i];
-        }
-        result = verification(code);
- 
+
         printf("#%d %d\n", test_case, result);
     }
+
     return 0;
 }
