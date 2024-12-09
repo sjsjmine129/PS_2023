f5ff45e533cd7dbf2042b3bdf6607126b49077b4
sjsjmine129 <sjsjmine129@gmail.com>
2024-12-09 16:40:20 +0900

    Fix

--- samsung_tests/12_9/().c
+++ samsung_tests/12_9/().c
@@ -1,100 +1,98 @@
 #include <iostream>
-#include <algorithm>
-#include <cstring>
 #include <vector>
+#include <string>
 using namespace std;
- 
-int flipCnt;
-vector<pair<int, int>> result;
-vector<int> subSumArr;
- 
-string rOperation(int left, int right, string str) {
-    int s = left, e = right;
+
+int flipCount;
+vector<pair<int, int>> operations;
+vector<int> cumulativeSumArray;
+
+string reverseAndFlip(int start, int end, string str) {
+    int left = start, right = end;
     while (left < right) {
-        char temp = str[left];
-        str[left] = str[right];
-        str[right] = temp;
-        left++; right--;
+        swap(str[left], str[right]);
+        left++;
+        right--;
     }
- 
-    for(int i=s; i<=e; i++){
-        if (str[i] == '(') str[i] = ')';
-        else if (str[i] == ')') str[i] = '(';
+
+    for (int i = start; i <= end; i++) {
+        str[i] = (str[i] == '(') ? ')' : '(';
     }
- 
+
     return str;
 }
- 
-string flip(string str, int len) {
-    int minValue = 0, pos = -1, sum = 0;
-    for (int i = 0; i < len; i++) {
-        if (str[i] == '(') sum++;
-        else if (str[i] == ')') sum--;
- 
-        if (sum < minValue) {
-            minValue = sum; // 음수이면서 제일 작은 값으로 갱신
-            pos = i; // 인덱스
+
+string ensureNonNegativePrefixSum(string str, int length) {
+    int minPrefixSum = 0, minIndex = -1, currentSum = 0;
+
+    for (int i = 0; i < length; i++) {
+        currentSum += (str[i] == '(') ? 1 : -1;
+
+        if (currentSum < minPrefixSum) {
+            minPrefixSum = currentSum;
+            minIndex = i;
         }
     }
- 
-    // 누적합이 음수인 경우가 존재하였다면
-    if (pos != -1) {
-        flipCnt++;
-        result.push_back(make_pair(0, pos));
-        str = rOperation(0, pos, str);
+
+    if (minIndex != -1) {
+        flipCount++;
+        operations.emplace_back(0, minIndex);
+        str = reverseAndFlip(0, minIndex, str);
     }
- 
+
     return str;
 }
- 
-void solve(string str, int len) {
-    if (len % 2 == 1) {
-        flipCnt = -1;
+
+void solve(string str, int length) {
+    if (length % 2 != 0) {
+        flipCount = -1;
         return;
     }
- 
-    // 각 구간합 원소가 음수가 없도록 처리
-    str = flip(str, len);
- 
-    // 구간 누적합
-    int sum = 0;
-    for (int i = 0; i < len; i++) {
-        if (str[i] == '(') sum++;
-        else if (str[i] == ')') sum--;
-        subSumArr.push_back(sum);
+
+    str = ensureNonNegativePrefixSum(str, length);
+
+    int currentSum = 0;
+    for (int i = 0; i < length; i++) {
+        currentSum += (str[i] == '(') ? 1 : -1;
+        cumulativeSumArray.push_back(currentSum);
     }
- 
-    if(subSumArr.back() == 0) return;
-    
-    // 누적합의 절반값을 가지는 누적합들 중 가장 마지막 인덱스 찾기
-    int halfValue = subSumArr.back() / 2;
-    int pos = -1;
-    for (int i = 0; i < len; i++) {
-        if (halfValue == subSumArr.at(i)) {
-            pos = i + 1;
+
+    if (cumulativeSumArray.back() == 0) return;
+
+    int targetSum = cumulativeSumArray.back() / 2;
+    int splitIndex = -1;
+
+    for (int i = 0; i < length; i++) {
+        if (cumulativeSumArray[i] == targetSum) {
+            splitIndex = i + 1;
         }
     }
-    result.push_back(make_pair(pos, len - 1));
-    flipCnt++;
+
+    operations.emplace_back(splitIndex, length - 1);
+    flipCount++;
 }
- 
-int main(void) {
-    int testCase;
-    cin >> testCase;
-    for (int tc = 1; tc <= testCase; tc++) {
-        int len; string str;
-        cin >> len >> str;
-        
-        flipCnt = 0;
-        subSumArr.clear();
-        result.clear();
- 
-        solve(str, len);
- 
-        printf("#%d %d\n", tc, flipCnt);
-        for (int i = 0; i < result.size(); i++) {
-            printf("%d %d\n", result[i].first, result[i].second);
+
+int main() {
+    int testCases;
+    cin >> testCases;
+
+    for (int t = 1; t <= testCases; t++) {
+        int length;
+        string sequence;
+
+        cin >> length >> sequence;
+
+        flipCount = 0;
+        cumulativeSumArray.clear();
+        operations.clear();
+
+        solve(sequence, length);
+
+        cout << "#" << t << " " << flipCount << "\n";
+        for (const auto& op : operations) {
+            cout << op.first << " " << op.second << "\n";
         }
     }
+
     return 0;
 }
